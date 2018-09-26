1ae5f6f6194edfaf84fe7bf59d0d7e0444e5e418
quang-le <quangle.bxl@gmail.com>
2018-09-26 12:00:41 +0200

    first draft dashboard data fetch

--- degroof-peter/src/data/data.jsx
+++ degroof-peter/src/data/data.jsx
@@ -1,67 +1,67 @@
-import React, {Component} from 'react';
-
-class Data extends Component{
-    constructor(props) {
-        super(props);
-        this.state={
-            users:[
-                {
-                    userId:1,
-                    firstName:"Jean-Michel",
-                    lastName: "React",
-                    city: "Bruxelles",
-                    zipCode:1130,
-                    phone:0477123456,
-                    totalHours:12,
-                    skills:["accounting", "finances"],
-                },
-                {
-                    userId:2,
-                    firstName:"Odile",
-                    lastName: "Deray",
-                    city: "Charleroi",
-                    zipCode: 7000,
-                    phone:0477456754,
-                    totalHours:12,
-                    skills:["IT", "finances"],
-                }
-            ],
-            projects:[
-                {
-                    projectId:1,
-                    projectName:"Loupiote",
-                    projectRegion:"Bruxelles",
-                    fields:["education","social"],
-                    skillsNeeded:["IT", "accounting"],
-                    hoursNeeded:15,
-                    type:"coaching",
-                    mission:"Mise à jour du site Internet et révision de la compta pour soumission au pouvoir subsidiant ",
-                },
-                {
-                    projectId:2,
-                    projectName:"Carabistouille",
-                    projectRegion:"Hainaut",
-                    fields:["social"],
-                    skillsNeeded:["accounting"],
-                    hoursNeeded:15,
-                    type:"mentoring",
-                    mission:"Révision du budget pour demande de subside européen ",
-                }
-            ],
-            fields:[
-                "education",
-                "social",
-                "environnement",
-                "entrepreneurship",
-            ],
-            skills:[
-                "accounting",
-                "IT",
-                "HR",
-                "finances"
-            ]
-        }
-    }
+var data={
     
-
-}
+    users:[
+        {
+            userId:1,
+            firstName:"Jean-Michel",
+            lastName: "React",
+            city: "Bruxelles",
+            zipCode:1130,
+            phone:0477123456,
+            totalHours:12,
+            skills:["accounting", "finances"],
+            applications:[],
+            selections:[],
+            login:"",
+            pwd:"",
+        },
+        {
+            userId:2,
+            firstName:"Odile",
+            lastName: "Deray",
+            city: "Charleroi",
+            zipCode: 7000,
+            phone:0477456754,
+            totalHours:12,
+            skills:["IT", "finances"],
+            applications:[],
+            selections:[],
+            login:"",
+            pwd:"",
+        }
+    ],
+    projects:[
+        {
+            projectId:1,
+            projectName:"Loupiote",
+            projectRegion:"Bruxelles",
+            fields:["education","social"],
+            skillsNeeded:["IT", "accounting"],
+            hoursNeeded:15,
+            type:"coaching",
+            mission:"Mise à jour du site Internet et révision de la compta pour soumission au pouvoir subsidiant ",
+        },
+        {
+            projectId:2,
+            projectName:"Carabistouille",
+            projectRegion:"Hainaut",
+            fields:["social"],
+            skillsNeeded:["accounting"],
+            hoursNeeded:15,
+            type:"mentoring",
+            mission:"Révision du budget pour demande de subside européen ",
+        }
+    ],
+    fields:[
+        "education",
+        "social",
+        "environnement",
+        "entrepreneurship",
+    ],
+    skills:[
+        "accounting",
+        "IT",
+        "HR",
+        "finances"
+    ]
+  }
--- degroof-peter/src/pages/Dashboard.js
+++ degroof-peter/src/pages/Dashboard.js
@@ -3,6 +3,22 @@ import { View, Text, Button } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 export default class Dashboard extends Component {
     render() {
+        let latestProjects= this.props.data.projects.map((project,index)=>{
+            return (
+                <View key={"project"+ project.projectId}> 
+                    <Text>project.name</Text>
+                    <Text>project.projectName</Text>
+                    <Text>project.projectRegion</Text>
+                    <Text>project.fields</Text>
+                    <Text>project.skillsNeeded</Text>
+                    <Text>project.hoursNeeded</Text>
+                    <Text>project.type</Text>
+                    <Applied projectId={project.projectId} activeUser={this.props.data.activeUser}/>
+                </View>
+            )
+
+        })
+
         return (
             <View>
                 <Button
