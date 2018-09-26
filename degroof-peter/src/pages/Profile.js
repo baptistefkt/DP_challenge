73b361266f79549be5270845be36219b3344dd13
baptistefkt <baptistefirket@gmail.com>
2018-09-26 16:19:59 +0200

    profile.js ok

--- degroof-peter/src/pages/Profile.js
+++ degroof-peter/src/pages/Profile.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { View, Text, Image, StyleSheet, Button, TouchableHighlight } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+// import { Actions } from 'react-native-router-flux';
 
 class Profile extends Component {
 
     render() {
         return (
-            <View>
-                <Button
-                    title='Go Back'
-                    onPress={() => { Actions.pop(); }}>
-                    <Text>Goto back</Text>
-                </Button>
-                <Text>
-                    This is Profile page.
-                </Text>
-
+            <View style={styles.container}>
+                <View style={styles.cardProfile}>
+                    <Text style={styles.name}>Prénom Nom</Text>
+                    <Image source={require('../assets/becode_logo.jpg')} style={styles.image}></Image>
+                </View>
+                <View style={styles.textContainer}>
+                    <View style={styles.adress}>
+                        <Text>
+                            Adress : 000 rue de blablabla
+                            1000 Bruxelles
+                        </Text>
+                    </View>
+                    <View style={styles.skills}>
+                        <Text>
+                            Skills : bla, bla, bla
+                        </Text>
+                    </View>
+                    <View style={styles.hours}>
+                        <Text>
+                            Hours : 14/24
+                        </Text>
+                    </View>
+                </View>
 
             </View>
         );
@@ -23,3 +36,48 @@ class Profile extends Component {
 }
 
 export default Profile;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "space-between",
+        backgroundColor: 'white',
+    },
+
+    cardProfile: {
+        flex: 1,
+        justifyContent: "space-around",
+    },
+
+    name: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#4DE6A1',
+    },
+
+    image: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+    },
+
+    textContainer: {
+        flex: 1,
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+
+    adress: {
+        flex: 2,
+
+    },
+
+    skills: {
+        flex: 1
+    },
+
+    hours: {
+        flex: 1
+    },
+})
