83d780d3b7f8cc2849b76ffc21265123fb8cc549
baptistefkt <baptistefirket@gmail.com>
2018-09-27 22:18:55 +0200

    ok

--- degroof-peter/App.js
+++ degroof-peter/App.js
@@ -10,78 +10,78 @@ export default class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state={
-        activeUser:{},
-        users:[
-            {
-                userId:1,
-                firstName:"Jean-Michel",
-                lastName: "React",
-                city: "Bruxelles",
-                zipCode:1130,
-                phone:"0477123456",
-                totalHours:12,
-                skills:["accounting", "finances"],
-                applications:[],
-                selections:[],
-                login:"jean",
-                pwd:"Aaa",
-            },
-            {
-                userId:2,
-                firstName:"Odile",
-                lastName: "Deray",
-                city: "Charleroi",
-                zipCode: 7000,
-                phone:"0477456754",
-                totalHours:12,
-                skills:["IT", "finances"],
-                applications:[1],
-                selections:[],
-                login:"odile.deray@gmail.com",
-                pwd:"odilederay",
-            }
-        ],
-        projects:[
-            {
-                projectId:1,
-                projectName:"Loupiote",
-                projectRegion:"Bruxelles",
-                fields:["education","social"],
-                skillsNeeded:["IT", "accounting"],
-                hoursNeeded:15,
-                type:"coaching",
-                mission:"Mise à jour du site Internet et révision de la compta pour soumission au pouvoir subsidiant ",
-            },
-            {
-                projectId:2,
-                projectName:"Carabistouille",
-                projectRegion:"Hainaut",
-                fields:["social"],
-                skillsNeeded:["accounting"],
-                hoursNeeded:15,
-                type:"mentoring",
-                mission:"Révision du budget pour demande de subside européen ",
-            }
-        ],
-        fields:[
-            "education",
-            "social",
-            "environnement",
-            "entrepreneurship",
-        ],
-        skills:[
-            "accounting",
-            "IT",
-            "HR",
-            "finances"
-        ]
-      }
+    this.state = {
+      activeUser: {},
+      users: [
+        {
+          userId: 1,
+          firstName: "Jean-Michel",
+          lastName: "React",
+          city: "Bruxelles",
+          zipCode: 1130,
+          phone: "0477123456",
+          totalHours: 12,
+          skills: ["accounting", "finances"],
+          applications: [],
+          selections: [],
+          login: "jean",
+          pwd: "Aaa",
+        },
+        {
+          userId: 2,
+          firstName: "Odile",
+          lastName: "Deray",
+          city: "Charleroi",
+          zipCode: 7000,
+          phone: "0477456754",
+          totalHours: 12,
+          skills: ["IT", "finances"],
+          applications: [1],
+          selections: [],
+          login: "odile.deray@gmail.com",
+          pwd: "odilederay",
+        }
+      ],
+      projects: [
+        {
+          projectId: 1,
+          projectName: "Loupiote",
+          projectRegion: "Bruxelles",
+          fields: ["education", "social"],
+          skillsNeeded: ["IT", "accounting"],
+          hoursNeeded: 15,
+          type: "coaching",
+          mission: "Mise à jour du site Internet et révision de la compta pour soumission au pouvoir subsidiant ",
+        },
+        {
+          projectId: 2,
+          projectName: "Carabistouille",
+          projectRegion: "Hainaut",
+          fields: ["social"],
+          skillsNeeded: ["accounting"],
+          hoursNeeded: 15,
+          type: "mentoring",
+          mission: "Révision du budget pour demande de subside européen ",
+        }
+      ],
+      fields: [
+        "education",
+        "social",
+        "environnement",
+        "entrepreneurship",
+      ],
+      skills: [
+        "accounting",
+        "IT",
+        "HR",
+        "finances"
+      ]
+    }
   }
 
-  activeUser(user){
+  activeUser(user) {
     Actions.Projects();
-    this.setState({activeUser: user})
+    this.setState({ activeUser: user })
   }
 
   render() {
@@ -89,9 +89,9 @@ export default class App extends Component {
       <Router hideNavBar="true">
         <Scene key="root">
           <Scene key="Login" component={Login} title="Login" initial={true} data={this.state} activeUser={this.activeUser.bind(this)} />
-          <Scene key="Dashboard" component={Dashboard} title="Dashboard" data={this.state}/>
-          <Scene key="Projects" component={Projects} title="Projects" data={this.state}/>
-          <Scene key="Profile" component={Projects} title="Profile" />
+          <Scene key="Dashboard" component={Dashboard} title="Dashboard" data={this.state} />
+          <Scene key="Projects" component={Projects} title="Projects" data={this.state} />
+          <Scene key="Profile" component={Profile} title="Profile" />
 
         </Scene>
       </Router>
--- degroof-peter/src/pages/Login.js
+++ degroof-peter/src/pages/Login.js
@@ -4,90 +4,91 @@ import { Input } from '../components/Input';
 import { Actions } from 'react-native-router-flux';
 
 class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      loginValueHolder:"jean",
-      pwdValueHolder:"Aaa",
+    this.state = {
+      loginValueHolder: "jean",
+      pwdValueHolder: "Aaa",
     }
   }
 
-    //this.props.activeUser({ton user})
-    
+  //this.props.activeUser({ton user})
 
 
 
-  getValues(){
-    const loginValueHolder=this.state.loginValueHolder;
-    const pwdValueHolder=this.state.pwdValueHolder;
+
+  getValues() {
+    const loginValueHolder = this.state.loginValueHolder;
+    const pwdValueHolder = this.state.pwdValueHolder;
     let isConnected = false;
-    this.props.data.users.map((user)=>{
+    this.props.data.users.map((user) => {
       if (user.login === loginValueHolder && user.pwd === pwdValueHolder) {
         isConnected = true;
         this.props.activeUser(user);
-      } 
+      }
     });
-    if(!isConnected){
-      alert('Bad user') 
-    }else{
-      setTimeout(()=>{
-        Actions.Projects()
-      },200)
+    if (!isConnected) {
+      alert('Bad user')
+    } else {
+      setTimeout(() => {
+        Actions.Dashboard()
+      }, 200)
     }
   }
 
-    render() {
-        return (
-      
-          <View style={{backgroundColor: 'white', paddingBottom: 120}}>
-            <Image source={require('../assets/dp.png')}
-            style={{width: 300, height: 150, resizeMode: 'contain', alignSelf: 'center'
-            }}/>
+  render() {
+    return (
+
+      <View style={{ backgroundColor: 'white', paddingBottom: 120 }}>
+        <Image source={require('../assets/dp.png')}
+          style={{
+            width: 300, height: 150, resizeMode: 'contain', alignSelf: 'center'
+          }} />
 
         <Text style={styles.text}>
           Email
             </Text>
-            <Input
-              placeholder='john@example.com'
-              value={this.state.loginValueHolder}
-              onChangeText={loginValueHolder => this.setState({loginValueHolder})}
-              />
-            <Text style={{alignSelf: 'center'}}>
-              Password
+        <Input
+          placeholder='john@example.com'
+          value={this.state.loginValueHolder}
+          onChangeText={loginValueHolder => this.setState({ loginValueHolder })}
+        />
+        <Text style={{ alignSelf: 'center' }}>
+          Password
             </Text>
-            <Input
-              placeholder='password'
-              value={this.state.pwdValueHolder}
-              onChangeText={pwdValueHolder => this.setState({pwdValueHolder})}
-            />
-            <TouchableHighlight
-                style ={{
-                    height: 40,
-                    width:160,
-                    borderRadius:10,
-                    backgroundColor : "#4DE6A1",
-                    marginLeft :50,
-                    marginRight:50,
-                    marginTop: 50,
-                    alignSelf: 'center'
-                }}>
-            <Button
-              title="Login"
-              onPress={this.getValues.bind(this)}
-              color='white'
-            />
-            </TouchableHighlight>
-            <View
-                style={{
-                  borderBottomColor: 'grey',
-                  borderBottomWidth: 1,
-                  marginTop: 20,
-                  width: 300,
-                  alignSelf: 'center'
-                }}
-              >
-              </View>
-          </View>
+        <Input
+          placeholder='password'
+          value={this.state.pwdValueHolder}
+          onChangeText={pwdValueHolder => this.setState({ pwdValueHolder })}
+        />
+        <TouchableHighlight
+          style={{
+            height: 40,
+            width: 160,
+            borderRadius: 10,
+            backgroundColor: "#4DE6A1",
+            marginLeft: 50,
+            marginRight: 50,
+            marginTop: 50,
+            alignSelf: 'center'
+          }}>
+          <Button
+            title="Login"
+            onPress={this.getValues.bind(this)}
+            color='white'
+          />
+        </TouchableHighlight>
+        <View
+          style={{
+            borderBottomColor: 'grey',
+            borderBottomWidth: 1,
+            marginTop: 20,
+            width: 300,
+            alignSelf: 'center'
+          }}
+        >
+        </View>
+      </View>
 
     );
   }
